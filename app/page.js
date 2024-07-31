92f56b9d907f36e95e9f3d1f431739234c6e6e0f
Allen <allenshamrock37@gmail.com>
2024-07-31 14:17:37 +0300

    {Feat}:Configured firebase store to store the states in the firebase  database

--- app/page.js
+++ app/page.js
@@ -1,95 +1,67 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { firestore } from "@/firebase";
+import { collection } from "firebase/firestore";
 
 export default function Home() {
+  const items = [
+    "Tomato",
+    "Potato",
+    "Onions",
+    "Kales",
+    "Parsley",
+    "Corriander",
+    "Ginger",
+    "Carrot",
+    "Cabbage",
+  ];
+
+  useEffect(()=>{
+    const items = collection(firestore,'pantry')
+    console.log(items)
+  },[])
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box
+      width="100vw"
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
+      display="flex"
+      flexDirection={"column"}
+    >
+      <Box border={"1px solid black"}>
+        <Box
+          width={"800px"}
+          height={"70px"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          bgcolor={"#eb9534"}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <Typography variant={"h3"} color={"#333"} textAlign={"center"}>
+            Pantry Item
+          </Typography>
+        </Box>
+        <Stack width={"800px"} height={"300px"} spacing={2} overflow={"auto"}>
+          {items.map((item, index) => (
+            <Box
+              key={index}
+              width={"100%"}
+              height={"300px"}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              bgcolor={"#D1D5DB"}
+            >
+              <Typography variant="h5" color={"#333"} textAlign={"center"}>
+                {/* Capitalizing the first letters of the words  */}
+                {item.charAt(0).toUpperCase() + item.slice(1)}
+              </Typography>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
+    </Box>
   );
 }
---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNRhUv0ZBvhMjZQNUeYIGRr9IaRxd_EDs",
+  authDomain: "pantryapp-d1628.firebaseapp.com",
+  projectId: "pantryapp-d1628",
+  storageBucket: "pantryapp-d1628.appspot.com",
+  messagingSenderId: "1018101482940",
+  appId: "1:1018101482940:web:8ce4f53fc8cbeefd46d7b9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app)
+
+export {app,firestore}
